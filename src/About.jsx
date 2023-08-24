6115169fc086c87c584088a65e30fa7d9f9e1100
vishal soni <vishsoni043@gmail.com>
2023-08-25 01:53:48 +0530

    feature - connected with linkedin

--- src/About.jsx
+++ src/About.jsx
@@ -3,19 +3,27 @@ export const About = () => {
     <div>
       <div className="C">
         <div>
-          {" "}
           <div className="D">
-            {" "}
-            <img src="me1.jpg" alt="vishal" className="me" />
+            <a
+              className="link"
+              href="https://www.linkedin.com/in/vishal-soni-b21a4a1b8"
+              target="-blank"
+            >
+              <img
+                src="me1.jpg"
+                alt="vishal"
+                title="Linkedin"
+                className="linkedin"
+              />
+            </a>
+
             <h1>
-              {" "}
-              <strong> —hi! </strong>{" "}
+              <strong> —hi! </strong>
             </h1>
           </div>
         </div>
         <div className="E">
           <p className="P">
-            {" "}
             I'm an ex-civil engineer who made a career transition to become a
             passionate <br /> React web developer. I was born and raised in
             Rajasthan and have been fortunate <br /> to embark on an exciting
@@ -24,15 +32,13 @@ export const About = () => {
           <p className="P">
             I love to solve problems and create user friendly web apps.
           </p>
-          <p className="P"> Browse through my latest projects,</p>{" "}
+          <p className="P"> Browse through my latest projects,</p>
           <div className="D">
-            {" "}
             <a
               className="link"
               href="https://pixyverse.netlify.app/"
               target="-blank"
             >
-              {" "}
               pixyverse <span className="link-span"> a social-media-app.</span>
             </a>
             <a
@@ -40,14 +46,22 @@ export const About = () => {
               href="https://jewells.netlify.app/"
               target="-blank"
             >
-              {" "}
               jewels <span className="link-span">a e-commerce-app.</span>
-            </a>{" "}
+            </a>
           </div>
-        </div>{" "}
-      </div>{" "}
+        </div>
+      </div>
+
       <span className="C-span">
-        <i>All work © Vishal Soni </i>{" "}
+        <a
+          href="https://www.linkedin.com/in/vishal-soni-b21a4a1b8"
+          target="-blank"
+          title="linkedin"
+          className="link-span"
+        >
+          {" "}
+          <i>All work © Vishal Soni </i>{" "}
+        </a>
       </span>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,16 @@ export const Home = () => {
             —Hi, I'm Vishal Soni, I'm an React <br /> Web Developer, who loves
             to develop <br /> imaginative apps and joyful experiences.
           </h1>
-          <span>
-            {" "}
-            <i>All work © Vishal Soni </i>{" "}
+          <span className="C-span">
+            <a
+              href="https://www.linkedin.com/in/vishal-soni-b21a4a1b8"
+              target="-blank"
+              title="linkedin"
+              className="link-span"
+            >
+              {" "}
+              <i>All work © Vishal Soni </i>{" "}
+            </a>
           </span>
         </div>
       </div>
