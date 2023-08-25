acfdbeb7c5fc217a49943474cb12b9ee001670a6
vishal soni <vishsoni043@gmail.com>
2023-08-25 11:36:04 +0530

    style - fixrd css

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,9 +2,7 @@ import { NavLink } from "react-router-dom/dist";
 export const Header = () => {
   return (
     <div>
-      {" "}
       <NavLink to="/">
-        {" "}
         <img
           width="100"
           height="100"
@@ -14,17 +12,13 @@ export const Header = () => {
         />{" "}
       </NavLink>
       <div className="A">
-        {" "}
         <span>
-          {" "}
           <NavLink className="NavLink" to="/">
-            {" "}
             HOME{" "}
           </NavLink>{" "}
         </span>
         <span>
           <NavLink className="NavLink" to="/about">
-            {" "}
             ABOUT{" "}
           </NavLink>
         </span>{" "}
